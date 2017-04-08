067c404fc3f782faf3737d192a5e5a867f465d52
Max Campbell <maxc@maxc.in>
2017-04-08 22:49:16 +0100

    updated update script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt-get install build-essential cmake pkg-config libjpeg8-dev libtiff4-dev libjasper-dev libpng12-dev libgtk2.0-dev libavcodec-dev libavformat-dev libswscale-dev libv4l-dev libatlas-base-dev gfortran python-pip python-imageing python2.7-dev git -y
-sudo pip install numpy
 git clone https://github.com/Itseez/opencv.git
 cd opencv
 git checkout 3.0.0
@@ -8,6 +7,8 @@ cd ..
 git clone https://github.com/Itseez/opencv_contrib.git
 cd opencv_contrib
 git checkout 3.0.0
+cd ..
+sudo pip install numpy
 cd opencv
 mkdir build
 cd build
