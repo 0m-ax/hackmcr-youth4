1cd955c308c208553f15beed12345b13e19253f5
Max Campbell <maxc@maxc.in>
2017-04-09 13:24:39 +0100

    updated info

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ io.on('connection', function (socket) {
         console.log(faces)
         var info = faces
             .filter((face)=>face.conf < 100)
-            .map((match)=>require('./photos/'+match.id+'/about.json'))
+            .map((match)=>{
+                return {
+                    "info":require('./photos/'+match.id+'/about.json'),
+                    "id":match.id,
+                    "conf":match.conf
+                }
+            })
             console.log(info)
         socket.broadcast.emit('faces-info',info)
     })
