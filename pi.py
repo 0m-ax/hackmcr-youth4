e15e90477839c38bb7c95cc3303815f38b32e221
Max Campbell <maxc@maxc.in>
2017-04-09 12:08:15 +0100

    added debug

--- pi.py
+++ pi.py
@@ -16,14 +16,14 @@ from socketIO_client import SocketIO, LoggingNamespace
 
 with SocketIO('localhost', 8080, LoggingNamespace) as socketIO:
     while True:
+        print "new image"
         rawCapture = PiRGBArray(camera)
         camera.capture(rawCapture, format="bgr")
         image = rawCapture.array
-        cv2.imshow("Image", image)
-        cv2.waitKey(0)
-        bwimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #bwimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = faceCascade.detectMultiScale(bwimage, 1.3, 5, minSize=(80, 80))
         output = []
+        print "{} faces".format(len(faces))
         for (x, y, w, h) in faces:
             predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
             print "face {} guessed with confidence {}".format(predicted,conf)
